45ddb50dbe609d51bb4b46279f2de24919ca3292
sj <orange@wordncode.com>
2022-11-22 19:36:18 +0900

    [orange] 자동차 메인

--- resource/js/common.js
+++ resource/js/common.js
@@ -2,10 +2,26 @@
 
 ($(function(){
     sideMenu();
+    imgSwitch();
 }));
 
 function sideMenu(){
     $('.side-menu__btn').click(function(){
         $(this).addClass('is-click');
     })
+}
+
+function imgSwitch(){
+    $('.fullpage-box__quickLink > a').each(function() {
+        var nowImg = $(this).find('img');  
+        var srcName = nowImg.attr('src');  
+        var newSrc = srcName.substring(0, srcName.lastIndexOf('.'));
+        $(this).hover(function() { 
+            $(this).find('img').attr('src', newSrc+ '-on.' + /[^.]+$/.exec(srcName)); 
+            $(this).addClass('on');
+        }, function() {
+            $(this).find('img').attr('src', newSrc + '.' + /[^.]+$/.exec(srcName)); 
+            $(this).removeClass('on');
+        });    
+    });
 }
